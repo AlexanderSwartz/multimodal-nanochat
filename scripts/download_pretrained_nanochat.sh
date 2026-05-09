adf92e550423fcf94e1c977725e03d520af91d9b
Alexander Swartz <alexanderswartz198@gmail.com>
2026-05-09 18:02:06 +0000

    update scripts; give executable perms

--- scripts/download_pretrained_nanochat.sh
+++ scripts/download_pretrained_nanochat.sh
@@ -3,19 +3,16 @@
 CACHE_DIR="$HOME/.cache/nanochat"
 echo "--- Saving pre-trained weights and tokenizer to $CACHE_DIR---"
 
-echo "--- Installing Hugging Face Hub ---"
-uv pip install huggingface_hub
-
 echo "--- Downloading Nanochat Checkpoints ---"
-huggingface-cli download sdobson/nanochat model_000650.pt \
-    --local-dir "$CACHE_DIR/chatsft_checkpoints/d20"
+hf download sdobson/nanochat model_000650.pt \
+    --local-dir "$CACHE_DIR/base_checkpoints/d20"
 
-huggingface-cli download sdobson/nanochat meta_000650.json \
-    --local-dir "$CACHE_DIR/chatsft_checkpoints/d20"
+hf download sdobson/nanochat meta_000650.json \
+    --local-dir "$CACHE_DIR/base_checkpoints/d20"
 
 echo "--- Downloading Tokenizer Files ---"
-huggingface-cli download sdobson/nanochat tokenizer.pkl \
+hf download sdobson/nanochat tokenizer.pkl \
     --local-dir "$CACHE_DIR/tokenizer"
 
-huggingface-cli download sdobson/nanochat token_bytes.pt \
+hf download sdobson/nanochat token_bytes.pt \
     --local-dir "$CACHE_DIR/tokenizer"
